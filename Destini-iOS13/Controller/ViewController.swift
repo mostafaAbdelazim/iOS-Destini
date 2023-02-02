e50bc9236482648e32419bbf12f6d8a1acadfb64
mostafaabdelazim <mzeemadev@gmail.com>
2023-02-02 20:26:18 +0200

    final commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet var storyTextLabel: UILabel!
+
+    @IBOutlet var choiceOneButton: UIButton!
+    @IBOutlet var choiceTwoButton: UIButton!
+
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        storyBrain.subitChoice(sender.currentTitle!)
+        updateUI()
+    }
 
+    func updateUI() {
+        storyTextLabel.text = storyBrain.getStoryTitle()
+        choiceOneButton.setTitle(storyBrain.getStoryFirstChoice(), for: .normal)
+        choiceTwoButton.setTitle(storyBrain.getStorySecondChoice(), for: .normal)
+    }
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1Text: String
+    var choice2Text: String
+    var choice1Destination: Int
+    var choice2Destination: Int
+    init(title: String, choice1: String, choice2: String, choice1Destination: Int, choice2Destination: Int) {
+        self.title = title
+        choice1Text = choice1
+        choice2Text = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
+    }
+}
